6935d53dfb049ea1aa63151c48ce594ce6069454
Vitta <wanglikun@shinetechchina.com>
2019-06-26 14:59:57 +0800

    Used NSURLSessionDataTask to check whether the network is avaliable

--- src/ios/WLKNetworkCheck.m
+++ src/ios/WLKNetworkCheck.m
@@ -1,37 +1,74 @@
 #import "WLKNetworkCheck.h"
 
 @implementation WLKNetworkCheck
--(void)initHostReachWithHostName:(CDVInvokedUrlCommand *)command
-{
-    NSArray *arguments = command.arguments;
-    NSString *hostName = [arguments count] > 0 ? arguments[0] : @"";
-    self.hostReach = [Reachability reachabilityWithHostName:hostName];
-    [self.hostReach startNotifier];
-    CDVPluginResult * result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @"Init host reach success"];
-    [self.commandDelegate sendPluginResult: result callbackId:command.callbackId];
+- (void)initHostReachWithHostName:(CDVInvokedUrlCommand *)command {
+  NSArray *arguments = command.arguments;
+  NSString *hostName = [arguments count] > 0 ? arguments[0] : @"";
+  self.hostReach = [Reachability reachabilityWithHostName:hostName];
+  [self.hostReach startNotifier];
+  CDVPluginResult *result =
+      [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                        messageAsString:@"Init host reach success"];
+  [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
-- (void)getCurrentNetAvailableWithHostName:(CDVInvokedUrlCommand *)command
-{
-    [self.commandDelegate runInBackground:^{
-      
-        NetworkStatus networkStatus = [self.hostReach currentReachabilityStatus];
-        BOOL netAvailable = true;
-        
-        switch (networkStatus) {
-            case NotReachable:
-            {
-                netAvailable = false;
-                break;
+- (void)getCurrentNetAvailableWithHostName:(CDVInvokedUrlCommand *)command {
+  [self.commandDelegate runInBackground:^{
+    NSArray *arguments = command.arguments;
+    NSString *hostName = [arguments count] > 0 ? arguments[0] : @"";
+    NSURL *url = [NSURL URLWithString:hostName];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session
+        dataTaskWithRequest:request
+          completionHandler:^(NSData *data, NSURLResponse *response,
+                              NSError *error) {
+            CDVPluginResult *result;
+            if (nil == error) {
+              NSDictionary *dict =
+                  [NSJSONSerialization JSONObjectWithData:data
+                                                  options:kNilOptions
+                                                    error:nil];
+
+              NSLog(@"%@", dict);
+              result = [CDVPluginResult
+                  resultWithStatus:CDVCommandStatus_OK
+                   messageAsString:[NSString stringWithFormat:@"%d", YES]];
+            } else {
+              result = [CDVPluginResult
+                  resultWithStatus:CDVCommandStatus_OK
+                   messageAsString:[NSString stringWithFormat:@"%d", NO]];
             }
-            default: {
-                netAvailable = true;
-                break;
-            }
-        }
-        CDVPluginResult * result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: [NSString stringWithFormat:@"%d", netAvailable]];
-        [self.commandDelegate sendPluginResult: result callbackId:command.callbackId];
-    }];
+            [self.commandDelegate sendPluginResult:result
+                                        callbackId:command.callbackId];
+          }];
+    [dataTask resume];
+  }];
 }
+
+//- (void)getCurrentNetAvailableWithHostName:(CDVInvokedUrlCommand *)command
+//{
+//    [self.commandDelegate runInBackground:^{
+//
+//        NetworkStatus networkStatus = [self.hostReach
+//        currentReachabilityStatus]; BOOL netAvailable = true;
+//
+//        switch (networkStatus) {
+//            case NotReachable:
+//            {
+//                netAvailable = false;
+//                break;
+//            }
+//            default: {
+//                netAvailable = true;
+//                break;
+//            }
+//        }
+//        CDVPluginResult * result = [CDVPluginResult
+//        resultWithStatus:CDVCommandStatus_OK messageAsString: [NSString
+//        stringWithFormat:@"%d", netAvailable]]; [self.commandDelegate
+//        sendPluginResult: result callbackId:command.callbackId];
+//    }];
+//}
 
 @end
